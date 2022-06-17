84130f948f3a729b47a3a09e0104156711f4feca
unknown <vidaguilar22102001@gmailcom>
2022-06-17 08:31:47 -0500

    Agregando proyecto prueba

--- prueba/registros/models.py
+++ prueba/registros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Alumnos(models.Model):
+    matricula = models.CharField(max_length=12)
+    nombre= models.TextField()
+    carrera= models.TextField()
+    turno= models.CharField( max_length=10)
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now_add=True)
+       
